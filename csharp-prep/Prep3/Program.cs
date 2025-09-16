8dab8e8070af160be658d247a6ff2876a9ec1795
ThePieDoctor <evanmarbar@gmail.com>
2025-09-16 17:51:35 -0600

    git commit -m

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int userguess = -1;
+
+        Console.WriteLine("What is the magic number?");
+        string number = Console.ReadLine();
+        int num = int.Parse(number);
+
+        while (userguess != num)
+        {
+            Console.WriteLine("What is your guess?");
+            string guess = Console.ReadLine();
+            userguess = int.Parse(guess);
+
+            if (userguess < num)
+            {
+                Console.WriteLine("Higher: Guess again.");
+            }
+            else if (userguess > num)
+            {
+                Console.WriteLine("Lower: Guess again.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
