ddc5362125ceb704e9ef23e0edc8344bff98ccf5
ThePieDoctor <evanmarbar@gmail.com>
2025-09-16 17:37:00 -0600

    git commit -m "Grade"

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        if (percent >= 90)
+        {
+            Console.WriteLine("You have an A.");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("You have a B.");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("You have a C.");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("You have a D.");
+        }
+        else
+        {
+            Console.WriteLine("You have an F.");
+        }
     }
 }
