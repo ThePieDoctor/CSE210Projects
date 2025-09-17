b6137e344b58228cc95c581b53c5db22209fa086
ThePieDoctor <evanmarbar@gmail.com>
2025-09-16 18:02:29 -0600

    git commit -m

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -26,14 +26,13 @@ class Program
         Console.WriteLine($"The sum is: {sum}");
         float average = ((float)sum) / numbers.Count;
         Console.WriteLine($"The average is: {average}");
-        
+
         int max = numbers[0];
 
         foreach (int number in numbers)
         {
             if (number > max)
             {
-                // if this number is greater than the max, we have found the new max!
                 max = number;
             }
         }
