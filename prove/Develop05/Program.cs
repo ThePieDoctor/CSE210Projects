e4aa70a97bced78b8febf7d50eede5ca4bdad264
ThePieDoctor <evanmarbar@gmail.com>
2025-10-16 11:06:17 -0600

    git commit -m

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,22 @@
+// Evan Barrett
+// Wk05 Eternal Quest Program
+
+// Creativity and Exceeding Requirements -
+// To exceed requirements, I have implemented the following:
+// 1. A save/load feature that correctly saves and reconstructs the different goal types.
+//    The format can be extended for new goal types in the future.
+// 2. The program displays a congratulatory message each time an event is recorded,
+//    improving the "gamification".
+// 3. The user interface is clear and provides continuous feedback, such as showing the updated
+//    point total immediately after an action.
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
 }
